25d2c547f565bb0faf7c529a9cea33972ea9db84
heitorsDev <heitorhilles@gmail.com>
2026-08-13 11:18:53 -0300

    chore: scaffold src/vendors/discord with opcodes and header constants

---
+++ src/vendors/discord.js
@@ -0,0 +1,9 @@
+export const HEADER_SIZE = 8;
+
+export const OPCODE_HANDSHAKE = 0;
+export const OPCODE_FRAME = 1;
+export const OPCODE_CLOSE = 2;
+export const OPCODE_PING = 3;
+export const OPCODE_PONG = 4;
+
+export const RATE_LIMIT_MS = 15000;
